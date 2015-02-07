13f4249cf9c3da2a07be0b69f9ce3e3562ac6d7b
Hay <hay@altctrlwkrs.com>
2015-02-07 15:43:06 +0000

    added a test class

---
+++ Git Test/HayTest.h
@@ -0,0 +1,13 @@
+//
+//  HayTest.h
+//  Git Test
+//
+//  Created by Hay on 07/02/2015.
+//  Copyright (c) 2015 AltCtrlWrks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HayTest : NSObject
+
+@end
---
+++ Git Test/HayTest.m
@@ -0,0 +1,13 @@
+//
+//  HayTest.m
+//  Git Test
+//
+//  Created by Hay on 07/02/2015.
+//  Copyright (c) 2015 AltCtrlWrks. All rights reserved.
+//
+
+#import "HayTest.h"
+
+@implementation HayTest
+
+@end
